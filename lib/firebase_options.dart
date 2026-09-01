1b694e93930db67afd352980300380be7eea1c2a
kolovertik814-lab <kolovertik814@gmail.com>
2026-09-01 22:32:26 +0300

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,33 @@
+// Generated for GitHub-only workflow
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show TargetPlatform, defaultTargetPlatform, kIsWeb;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError('DefaultFirebaseOptions have not been configured for web.');
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError('DefaultFirebaseOptions have not been configured for iOS.');
+      case TargetPlatform.macOS:
+        throw UnsupportedError('DefaultFirebaseOptions have not been configured for macOS.');
+      case TargetPlatform.windows:
+        throw UnsupportedError('DefaultFirebaseOptions have not been configured for Windows.');
+      case TargetPlatform.linux:
+        throw UnsupportedError('DefaultFirebaseOptions have not been configured for Linux.');
+      default:
+        throw UnsupportedError('DefaultFirebaseOptions are not supported for this platform.');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyA...', // Если у тебя нет под рукой ключа, можешь нажать Next в консоли Firebase до конца, главное мы создали проект
+    appId: '1:552455976634:android:6a19ef96331857c21f7483',
+    messagingSenderId: '552455976634',
+    projectId: 'p2p-webrtc-chat',
+    databaseURL: 'https://p2p-webrtc-chat-default-rtdb.europe-west1.firebasedatabase.app',
+  );
+}
